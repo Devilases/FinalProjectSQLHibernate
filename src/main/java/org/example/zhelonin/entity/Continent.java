ab06a9139a747c92a7cfe62ad0d5d87e9d3bd71e
Devilases <zhelonin.2@yandex.ru>
2023-12-18 17:40:56 +0300

    upd: database queries implemented

---
+++ src/main/java/org/example/zhelonin/entity/Continent.java
@@ -0,0 +1,14 @@
+package org.example.zhelonin.entity;
+
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public enum Continent {
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
